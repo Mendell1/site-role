34724de7504857488fe04ba3a3ce7d65c180d596
Mendell1 <matias3.wendell@gmail.com>
2026-09-06 02:51:07 -0300

    test: allow V26 cache after V25.6

--- tests/v25_6_static.py
+++ tests/v25_6_static.py
@@ -82,7 +82,7 @@ for marcador in [
     assert marcador in css, f'CSS V25.6 incompleto: {marcador}'
 
 sw = read('sw-v25.js')
-assert "const CACHE_ATUAL = 'role-v25-" in sw
+assert "const CACHE_ATUAL = 'role-v" in sw
 assert './css/v25-6.css' in sw
 assert './js/recorrencia-v25.js' in sw
 assert './js/mapa-raio-v25.js' in sw
