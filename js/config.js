e14cf201ebf72343c7bffe8678bbd5b8de194011
Mendell1 <matias3.wendell@gmail.com>
2026-09-09 00:46:04 -0300

    Carrega prévia quadrada da foto do perfil

--- js/config.js
+++ js/config.js
@@ -108,6 +108,7 @@ if (SUPABASE_URL.startsWith('COLE')) {
     estiloLocal('css/perfil-switches-v26.css?v=20260908-0330','v26-perfil-switches');
     estiloLocal('css/perfil-avatar-card-v26.css?v=20260908-2130','v26-perfil-avatar-card');
     estiloLocal('css/perfil-foto-conta-v26.css?v=20260908-2130','v26-perfil-foto-conta');
+    estiloLocal('css/perfil-foto-quadrada-v26.css?v=20260909-0045','v26-perfil-foto-quadrada');
     scriptLocal('js/perfil-foto-conta-v26.js?v=20260908-2130','v26-perfil-foto-conta');
     scriptLocal('js/perfil-recuperacao-v26.js?v=20260908-0425','v26-perfil-recuperacao');
   }
